38698d40cfbc5331d7ab6aae948fb6d00b1502af
rh7112 <rh25170@gmail.com>
2025-07-19 03:20:30 -0400

    Fixing ecosystem.config again. Hopefully it works this time. Casing on port was incorrect, maybe?

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
-module_exports = {
-	apps: [
-		{
-			name: "personal-portfolio", 
-			cwd: "/mnt/nas/rh25170/Documents/personal-portfolio",
-			script: "npm",
-			args: "start",
-			watch: false,
-			env: {
-				NODE_ENV: "production",
-				port: 3000
-			}
-			}
-	]
+module.exports = {
+  apps: [
+    {
+      name: "personal-portfolio",
+      cwd: "/mnt/nas/rh25170/Documents/personal-portfolio",
+      script: "npm",
+      args: "start",
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
 };
