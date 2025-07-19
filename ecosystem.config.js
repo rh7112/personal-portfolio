e878403305f3487a42fa6e183e18c20b6de8f446
rh7112 <rh25170@gmail.com>
2025-07-19 02:50:51 -0400

    Fixing a few dependencies and adding an ecosystem.config.js to simplify the pm2 process on the raspberry pi.

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "personal-portfolio",
+      cwd: "/mnt/nas/rh25170/Documents/personal-portfolio",
+      script: "npm",
+      args: "start",
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ]
+};
